ed01c943f66630cf0e2af529703f506a6af41c4a
Lordx1 <felipe.marcelino19@hotmail.com>
2016-04-29 22:25:18 -0300

    Change Folder

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-import command.GeneratePlayers;
-import command.ClearHandPlayers;
-import command.DrawCardFromDeck;
-import command.GetDeck;
-import command.RotateDealer;
-import command.ShuffleDeck;
-import deck.Card;
-import player.Player;
 
 public class Main {
 
--- src/command/Command.java
+++
@@ -1,7 +0,0 @@
-package command;
-
-public interface Command <T> {
-	
-	public T execute();
-	
-}
